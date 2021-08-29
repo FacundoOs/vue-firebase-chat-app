5b16977fead6dc04db8a1b08d902f80132819c08
facundo.osores <facundo.osores@healthintegrator.eu>
2021-08-29 20:40:45 +0200

    vue app with firebase authentication

---
+++ src/firebase.js
@@ -0,0 +1,62 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+import Filter from 'bad-words'
+import { ref, onUnmounted, computed } from 'vue'
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: 'vue-chat-82855.firebaseapp.com',
+  projectId: 'vue-chat-82855',
+  storageBucket: 'vue-chat-82855.appspot.com',
+  messagingSenderId: '481990010881',
+  appId: '1:481990010881:web:845b14a79fcfc6a09d7800'
+})
+
+const auth = firebase.auth()
+
+export function useAuth() {
+  const user = ref(null)
+  const unsubscribe = auth.onAuthStateChanged(_user => (user.value = _user))
+  onUnmounted(unsubscribe)
+  const isLogin = computed(() => user.value !== null)
+
+  const signIn = async () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider()
+    await auth.signInWithPopup(googleProvider)
+  }
+  const signOut = () => auth.signOut()
+
+  return { user, isLogin, signIn, signOut }
+}
+
+const firestore = firebase.firestore()
+const messagesCollection = firestore.collection('messages')
+const messagesQuery = messagesCollection.orderBy('createdAt', 'desc').limit(100)
+const filter = new Filter()
+
+export function useChat() {
+  const messages = ref([])
+  const unsubscribe = messagesQuery.onSnapshot(snapshot => {
+    messages.value = snapshot.docs
+      .map(doc => ({ id: doc.id, ...doc.data() }))
+      .reverse()
+  })
+  onUnmounted(unsubscribe)
+
+  const { user, isLogin } = useAuth()
+  const sendMessage = text => {
+    if (!isLogin.value) return
+    const { photoURL, uid, displayName } = user.value
+    messagesCollection.add({
+      userName: displayName,
+      userId: uid,
+      userPhotoURL: photoURL,
+      text: filter.clean(text),
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+  }
+
+  return { messages, sendMessage }
+}
